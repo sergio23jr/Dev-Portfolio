4dcd71b3fdc1654b7bbed954b0d140953aa43134
Sergio Jr <sergio23jr@gmail.com>
2019-01-18 01:36:54 -0600

    links not working

--- gulpfile.js
+++ gulpfile.js
@@ -20,46 +20,30 @@ const banner = ['/*!\n',
 ].join('');
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function(cb) {
+gulp.task('vendor', function (cb) {
 
   // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
+  gulp.src(['./node_modules/bootstrap/dist/**/*', '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+    '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+  ]).pipe(gulp.dest('./vendor/bootstrap'))
 
   // Font Awesome
-  gulp.src([
-      './node_modules/@fortawesome/**/*',
-    ])
-    .pipe(gulp.dest('./vendor'))
+  gulp.src(['./node_modules/@fortawesome/**/*',
+  ]).pipe(gulp.dest('./vendor'))
 
   // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery'))
+  gulp.src(['./node_modules/jquery/dist/*', '!./node_modules/jquery/dist/core.js'
+  ]).pipe(gulp.dest('./vendor/jquery'))
 
   // jQuery Easing
-  gulp.src([
-      './node_modules/jquery.easing/*.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery-easing'))
+  gulp.src(['./node_modules/jquery.easing/*.js']).pipe(gulp.dest('./vendor/jquery-easing'))
 
   // Magnific Popup
-  gulp.src([
-      './node_modules/magnific-popup/dist/*'
-    ])
-    .pipe(gulp.dest('./vendor/magnific-popup'))
+  gulp.src(['./node_modules/magnific-popup/dist/*']).pipe(gulp.dest('./vendor/magnific-popup'))
 
   // Scrollreveal
-  gulp.src([
-      './node_modules/scrollreveal/dist/*.js'
-    ])
-    .pipe(gulp.dest('./vendor/scrollreveal'))
+  gulp.src(['./node_modules/scrollreveal/dist/*.js'
+  ]).pipe(gulp.dest('./vendor/scrollreveal'))
 
   cb();
 
